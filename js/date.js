f54105ecbae99b504911f5158fccbf841c8351f4
MarcoFonti <theemporiumg@gmail.com>
2024-01-16 15:33:51 +0100

    add snack 1

--- js/date.js
+++ js/date.js
@@ -1,2 +1,20 @@
 // CHECK 
 console.log ('JS OK DATI');
+
+// TAVOLO
+const tableName = 'Tavolo Vip';
+
+// INIVITATI
+const guests = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+];
+
--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
 // CHECK 
 console.log ('JS OK');
+
+
+// PRENDO IL METODO MAP PER CREARE UN NUOVO ARRAY CON LE INFORMAZIONI GIA' DISPONIBILI
+const ArraysList = guests.map((element, index) => {
+
+    // CREO UNA LISTA OGGETTI
+    return guestsList = 
+    
+        {
+            table: tableName,
+            name: element,
+            place: index + 1
+
+        }
+    
+
+});
+
+// STAMPO 
+console.log(ArraysList);
+
+
+   
+
+
+
+
+
+
+
